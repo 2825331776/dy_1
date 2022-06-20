6148d7baaa181e2dd270ba63218b21948d987196
吴长城 <781723432@qq.com>
2022-06-20 10:43:36 +0800

    分支：dyt_0616_victor Module： 版本：胜利定制版，修改了用户手册滑动，标识箭头隐藏与显示的问题，用户手册界面只有竖屏显示。 上传时间：2022年6月20日10:42:14

--- app/src/main/java/com/dyt/wcc/victor/ui/pdfPreview/PdfActivity.java
+++ app/src/main/java/com/dyt/wcc/victor/ui/pdfPreview/PdfActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Environment;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Toast;
+
+import androidx.viewpager.widget.ViewPager;
 
 import com.dyt.wcc.common.base.BaseActivity;
 import com.dyt.wcc.victor.R;
@@ -36,7 +39,7 @@ public class PdfActivity extends BaseActivity<ActivityPdfBinding> {
 		return R.layout.activity_pdf;
 	}
 
-	private String       fileName = "";
+	private String fileName = "";
 
 	@Override
 	protected void onDestroy () {
@@ -47,15 +50,41 @@ public class PdfActivity extends BaseActivity<ActivityPdfBinding> {
 	final String[] sampleAssets = {"SLReadMeCN.pdf", "SLReadMeEN.pdf"};
 	File pdfFolder;
 
-	protected void copyAssetsOnSDCard() {
+	protected void copyAssetsOnSDCard () {
 		CopyAsset copyAsset = new CopyAssetThreadImpl(getApplicationContext(), new Handler(), new CopyAsset.Listener() {
 			@Override
-			public void success(String assetName, String destinationPath) {
+			public void success (String assetName, String destinationPath) {
 				mDataBinding.pdfViewPager.setPdfPath(getPdfPathOnSDCard());
+
+				mDataBinding.ivLeft.setVisibility(View.GONE);
+				mDataBinding.pdfViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+					@Override
+					public void onPageScrolled (int position, float positionOffset, int positionOffsetPixels) {
+					}
+
+					@Override
+					public void onPageSelected (int position) {
+						if (position == 0) {
+							mDataBinding.ivLeft.setVisibility(View.GONE);
+							mDataBinding.ivRight.setVisibility(View.VISIBLE);
+						} else if (position == mDataBinding.pdfViewPager.getAdapter().getCount() - 1) {
+							mDataBinding.ivLeft.setVisibility(View.VISIBLE);
+							mDataBinding.ivRight.setVisibility(View.GONE);
+						} else {
+							mDataBinding.ivLeft.setVisibility(View.VISIBLE);
+							mDataBinding.ivRight.setVisibility(View.VISIBLE);
+						}
+					}
+
+					@Override
+					public void onPageScrollStateChanged (int state) {
+
+					}
+				});
 			}
 
 			@Override
-			public void failure(Exception e) {
+			public void failure (Exception e) {
 				e.printStackTrace();
 				Toast.makeText(mContext.get(), e.getMessage(), Toast.LENGTH_LONG).show();
 			}
@@ -66,7 +95,7 @@ public class PdfActivity extends BaseActivity<ActivityPdfBinding> {
 		}
 	}
 
-	protected String getPdfPathOnSDCard() {
+	protected String getPdfPathOnSDCard () {
 		fileName = "SLReadMeCN.pdf";
 		sp = mContext.get().getSharedPreferences(DYConstants.SP_NAME, Context.MODE_PRIVATE);
 		if (sp.getInt(DYConstants.LANGUAGE_SETTING_INDEX, 0) == 1) {
