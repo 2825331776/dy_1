7f8c4e278f2cb9f726fd5e9ec877cb9857eded1f
吴长城 <781723432@qq.com>
2022-11-12 18:19:46 +0800

    分支：dyt_targetApi31_0825 Module： 版本：VersionName:1.4.06 , VersionCode:235 模块新增，架构调整： 上传时间：2022年11月12日18:04:17

---
+++ BaseJniLib/Readme.md
@@ -0,0 +1,8 @@
+职责：
+1、生成一个base_jni.so ，图库 及其 app module 通过 baselib 或者其他module（也可自己生成一个utils）引用。 
+    base_jni:
+            AES加解密
+            图片结构体，自定义数据的 拆封
+            色板解析提取。
+            socket tools helper
+            
---
+++ Build_So/src/main/cpp/Build_So.cpp
@@ -0,0 +1,17 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("Build_So");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("Build_So")
+//      }
+//    }
